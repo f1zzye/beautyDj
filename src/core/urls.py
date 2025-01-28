2e04b2922dfc3a8429982c084bada24fba13f8f3
f1zzye <kosenko2401@gmail.com>
2025-01-28 17:06:07 +0200

    add ability to dynamically add products and categories to the template

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 
-from core.views import index, product_list
+from core.views import index, product_list, category_product_list
 
 app_name = "core"
 
 urlpatterns = [
     path("", index, name="index"),
     path("products/", product_list, name="product-list"),
+
+    path('category/<cid>/', category_product_list, name='category-product-list'),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,22 +1,39 @@
 from django.shortcuts import render
-
+from django.db.models import Count, Avg
 from core.models import (Address, CartOrder, CartOrderItems, Category, Coupon,
                          Product, ProductImages, WishList)
 
 
 def index(request):
     products = Product.objects.filter(product_status="опубліковано", featured=True)
+    categories = Category.objects.all().annotate(product_count=Count('category'))
 
     context = {
         "products": products,
+        'categories': categories,
     }
     return render(request, "index.html", context)
 
 
 def product_list(request):
     products = Product.objects.filter(product_status="опубліковано")
+    categories = Category.objects.all().annotate(product_count=Count('category'))
 
     context = {
         "products": products,
+        "categories": categories,
     }
     return render(request, "core/product_list.html", context)
+
+
+def category_product_list(request, cid):
+    category = Category.objects.get(cid=cid)
+    categories = Category.objects.all().annotate(product_count=Count('category'))
+    products = Product.objects.filter(product_status='опубліковано', category=category)
+
+    context = {
+        'category': category,
+        'products': products,
+        'categories': categories,
+    }
+    return render(request, 'core/category-product-list.html', context)
